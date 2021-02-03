3401abf0a445ed17ca04fa7c3f0787ddcc69d19e
chiranjibi.lawati@infodeveloper.com.np <chiranjibilawati08@gmail.com>
2021-02-03 21:56:33 +0545

    post form created

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -15,3 +15,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('contact','PagesController@getContact');
 Route::get('about', 'PagesController@getAbout');
 Route::get('/', 'PagesController@getIndex');
+
+Route::resource('posts', 'PostController');
